0a95022e799c418ade63879e97eed98c8e197cc1
Ayush4398 <agarwalayush1998@gmail.com>
2021-11-17 22:29:50 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -11,7 +11,7 @@ function contact(){
 
     <a href="mailto: agarwalayush1998@gmail.com"><img class="contacticon" src="doc/gmail.jpg" alt=""/></a>
     <a href="https://www.linkedin.com/in/ayush-agrawal-56b48b216/"><img class="contacticon" src="doc/linkedin.png" alt=""/></a>
-    <a href="https://github.com/Ayushpicturesque"><img class="contacticon" src="doc/github.png" alt=""/></a>
+    <a href="https://https://github.com/Ayush4398"><img class="contacticon" src="doc/github.png" alt=""/></a>
     <a href="https://www.snapchat.com/add/agarwalayush04"><img class="contacticon" src="doc/snap.jpg" alt=""/></a>
     <a href="https://www.instagram.com/ayush43998/"><img class="contacticon" src="doc/insta.jpg" alt=""/></a>
     
