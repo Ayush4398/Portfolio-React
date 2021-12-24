2d0b8af77812aa72b27c67b39a7fe2a54efa6b11
Ayush4398 <agarwalayush1998@gmail.com>
2021-12-24 12:27:31 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,7 +9,7 @@ function home(){
                  <h1>Hello!</h1>
                 </div>
                 <div className="col-lg-6">
-                <a className="text-decoration-none copyright" href="https://drive.google.com/file/d/1ebF328u8bTGy7fpdTsvZkcM-Avn3dWgg/view?usp=sharing"><h1>Resume Link</h1></a>
+                <a className="text-decoration-none copyright" href="https://drive.google.com/file/d/15Ipk3uf0TXXnDU5fXqGMX8S8c2gEaTxm/view?usp=sharing"><h1>Resume Link</h1></a>
                 <h3 className="introline">I'm a web designer and developer who can make a website for you as per your needs, whatever the layout you want, whatever the way you want.</h3>
                 </div>
             </div>
